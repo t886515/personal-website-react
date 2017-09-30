7626a84059da9dac3b318c5d8f57840d05504c18
Lina Yang <t886515@hotmail.com>
2017-09-29 21:09:46 -0500

    Set-up image serving config - Have the titleName image succesfully displayed on the webpage.

--- src/client/components/App.jsx
+++ src/client/components/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import test from '../images/titlenamesh.png'
 
 export default class App extends React.Component {
   render() {
     return (
      <div style={{textAlign: 'center'}}>
         <h1>Lina Yang</h1>
+        <img src={test}/>
       </div>
     );
   }
--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,36 @@
 const path = require('path');
-
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
-  template: './client/index.html',
+  template: './src/client/index.html',
   filename: 'index.html',
-  inject: 'body'
+  inject: 'body',
+
 })
 
 
 module.exports = {
-  entry: './client/index.js',
+  entry: './src/client/index.js',
   output: {
     path: path.resolve('dist'),
     filename: 'index_bundle.js'
   },
   module: {
-    loaders: [
-      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
-      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
+    rules: [
+      { test: /\.js$/, use: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.jsx$/, use: 'babel-loader', exclude: /node_modules/ },
+      { use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: 'css-loader'
+        }), 
+        test: /\.css$/
+      },
+      { test: /\.(jpeg|png|gif|svg|JPG|pdf|ico)$/, use: [{loader: 'url-loader', options: {limit: 40000}}, 'image-webpack-loader']}
     ]
   },
-  plugins: [HtmlWebpackPluginConfig]
+  plugins: [
+    HtmlWebpackPluginConfig,
+    new ExtractTextPlugin('style.css')
+  ]
 }
