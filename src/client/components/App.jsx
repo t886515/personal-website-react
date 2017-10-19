bd5f9461f67a2589c0ca6d0e484a85ffc5d88c4f
Lina Yang <t886515@hotmail.com>
2017-10-19 17:39:09 -0500

    added about, resume and home route, client-side routing set-up completion

--- src/client/components/App.jsx
+++ src/client/components/App.jsx
@@ -1,13 +1,28 @@
 import React from 'react';
-import test from '../images/titlenamesh.png'
+import titleName from '../images/titlenamesh.png';
+import logo from '../images/namelogo2.png';
+import { BrowserRouter, Route, Link, Switch, withRouter } from 'react-router-dom';
+import About from './About.jsx';
+import Home from './Home.jsx';
+import Resume from './Resume.jsx';
 
-export default class App extends React.Component {
+class App extends React.Component {
   render() {
     return (
-     <div style={{textAlign: 'center'}}>
-        <h1>Lina Yang</h1>
-        <img src={test}/>
-      </div>
+      <BrowserRouter>
+        <div>
+          <div style={{ backgroundColor: 'black' }}>
+            <img style={{width: '5%', height: '5%'}} src={logo}/>
+          </div>
+          <Switch>
+            <Route exact path="/" render={()=><Home />} />
+            <Route path="/about" render={()=><About />} />
+            <Route path="/resume" render={()=><Resume />} />
+          </Switch>
+        </div>
+      </BrowserRouter>
     );
   }
 }
+
+export default App;
