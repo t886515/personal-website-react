3035912481bacdf34cde834da5cf6f1340533d96
Lina Yang <t886515@hotmail.com>
2017-10-19 17:41:11 -0500

    tested out client-side routing - code stayed unchanged

--- src/client/index.js
+++ src/client/index.js
@@ -1,5 +1,18 @@
+// import 'babel-polyfill';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
+// import { AppContainer } from 'react-hot-loader';
+
+// import { BrowserRouter } from 'react-router-dom';
+
+// const wrapApp = AppComponent =>
+//   <AppContainer>
+//     <BrowserRouter>
+//       <AppComponent />
+//     </BrowserRouter>
+//   </AppContainer>
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
