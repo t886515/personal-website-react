803e5541d73e601ccca4845a9ed2f926a9170014
Lina Yang <t886515@hotmail.com>
2017-10-20 11:48:31 -0500

    added transitiongroupcss and allowed enter animation

--- src/client/components/Home.jsx
+++ src/client/components/Home.jsx
@@ -1,23 +1,35 @@
 import React from 'react';
 import titleName from '../images/titlenamesh.png';
 import { BrowserRouter, Route, Link, Switch, withRouter } from 'react-router-dom';
-
+import { CSSTransitionGroup } from 'react-transition-group';
 
 class Home extends React.Component {
+  componentDidMount() {
+    console.log("how many times in home")
+  }
+
   render() {
     return (
+      <CSSTransitionGroup
+      transitionName="example"
+      transitionAppear={true}
+      transitionAppearTimeout={900}
+      transitionEnter={false}
+      transitionLeave={false}>
       <div>
           <div className="centerDiv">
             <div className="centerName"> LINA YANG </div>
             <div className="centerTitle"> FULL-STACK DEVELOPER・UX/UI DESIGNER・WEB-DEVELOPER </div>
+            <br />
           <div className="navLinkGroup">
-            <Link className="navlink" to="/about">About</Link>。
-            <Link className="navlink" to="/resume"> Resume </Link>。
-            <Link className="navlink" to="/project"> Projects </Link>。
-            <Link className="navlink" to="/contact"> Contact Me </Link>
+            <Link className="navlink" to="/about"><i className="fa fa-id-card-o fa-2x" aria-hidden="true"></i></Link>。
+            <Link className="navlink" to="/resume"> <i className="fa fa-file-o fa-2x" aria-hidden="true"></i> </Link>。
+            <Link className="navlink" to="/project"> <i className="fa fa-wrench fa-2x" aria-hidden="true"></i> </Link>。
+            <Link className="navlink" to="/contact"> <i className="fa fa-envelope-open-o fa-2x" aria-hidden="true"></i> </Link>
             </div>
           </div>
       </div>
+      </CSSTransitionGroup>
     );
   }
 }
