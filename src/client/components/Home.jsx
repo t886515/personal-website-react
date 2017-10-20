74979d7b378e19601c2e1f13f4dbdcd37627aa99
Lina Yang <t886515@hotmail.com>
2017-10-19 19:44:42 -0500

    -remove style object and handle styling in style.css -finish all links

--- src/client/components/Home.jsx
+++ src/client/components/Home.jsx
@@ -2,14 +2,31 @@ import React from 'react';
 import titleName from '../images/titlenamesh.png';
 import { BrowserRouter, Route, Link, Switch, withRouter } from 'react-router-dom';
 
+// const styles = {
+//   centerDiv: {
+//     textAlign: 'center'
+//   },
+//   navlink: {
+//     textDecoration: 'none',
+//     color: '#212121',
+//     width: '80%',
+//     fontSize: '20px',
+//     fontFamily: '"Lucida Sans Unicode", "Lucida Grande", sans-serif'
+//   }
+// }
+
 class Home extends React.Component {
   render() {
     return (
       <div>
-          <div style={{textAlign: 'center'}}>
+          <div className="centerDiv">
             <img src={titleName}/>
-            <Link to="/about">About</Link>
-            <Link to="/resume">
+            <div className="testing">
+            <Link className="navlink" to="/about">About</Link>。
+            <Link className="navlink" to="/resume"> Resume </Link>。
+            <Link className="navlink" to="/project"> Projects </Link>。
+            <Link className="navlink" to="/contact"> Contact Me </Link>
+            </div>
           </div>
       </div>
     );
