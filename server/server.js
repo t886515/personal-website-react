078c3262a4aff342250c4f531037d9db4a027649
Lina Yang <t886515@hotmail.com>
2017-10-19 23:15:36 -0500

    set-up basic server serving css

--- server/server.js
+++ server/server.js
@@ -3,8 +3,7 @@ import express from 'express';
 import cors from 'cors';
 import logger from 'morgan';
 import parser from 'body-parser';
-import './db/config';
-import router from './router';
+
 
 import { APP_NAME, STATIC_PATH, WEB_PORT } from '../shared/config';
 import { isProd } from '../shared/util';
