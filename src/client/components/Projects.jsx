aa57fbd2bff2e8d23869aa116c6c29dab06afa96
Lina Yang <t886515@hotmail.com>
2017-10-19 22:41:51 -0500

    initiate project components

---
+++ src/client/components/Projects.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter, Route, Link, Switch, withRouter } from 'react-router-dom';
+
+class Projects extends React.Component {
+  constructor(props) {
+    super(props)
+    console.log('hihihi')
+  }
+  render() {
+    return (
+      <div> 
+      Projectsfdsafdsafd
+      </div>
+    );
+  }
+}
+
+export default withRouter(Projects);
