0fedbb8eb03046a5c1ff887ce4de853322b2f276
Lina Yang <t886515@hotmail.com>
2017-10-19 23:12:04 -0500

    instead of using img, useing text for name/titles

--- src/client/components/Home.jsx
+++ src/client/components/Home.jsx
@@ -2,26 +2,15 @@ import React from 'react';
 import titleName from '../images/titlenamesh.png';
 import { BrowserRouter, Route, Link, Switch, withRouter } from 'react-router-dom';
 
-// const styles = {
-//   centerDiv: {
-//     textAlign: 'center'
-//   },
-//   navlink: {
-//     textDecoration: 'none',
-//     color: '#212121',
-//     width: '80%',
-//     fontSize: '20px',
-//     fontFamily: '"Lucida Sans Unicode", "Lucida Grande", sans-serif'
-//   }
-// }
 
 class Home extends React.Component {
   render() {
     return (
       <div>
           <div className="centerDiv">
-            <img src={titleName}/>
-            <div className="testing">
+            <div className="centerName"> LINA YANG </div>
+            <div className="centerTitle"> FULL-STACK DEVELOPER・UX/UI DESIGNER・WEB-DEVELOPER </div>
+          <div className="navLinkGroup">
             <Link className="navlink" to="/about">About</Link>。
             <Link className="navlink" to="/resume"> Resume </Link>。
             <Link className="navlink" to="/project"> Projects </Link>。
