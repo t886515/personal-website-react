0907c5c67024b32fd4e0a94754218128f3546fa0
Lina Yang <t886515@hotmail.com>
2017-10-19 22:44:00 -0500

    edit minor syntax error

--- src/client/components/App.jsx
+++ src/client/components/App.jsx
@@ -5,6 +5,8 @@ import { BrowserRouter, Route, Link, Switch, withRouter } from 'react-router-dom
 import About from './About.jsx';
 import Home from './Home.jsx';
 import Resume from './Resume.jsx';
+import ContactMe from './ContactMe.jsx';
+import Projects from './Projects.jsx'
 
 class App extends React.Component {
   render() {
@@ -12,12 +14,14 @@ class App extends React.Component {
       <BrowserRouter>
         <div className="homeDiv">
           <div style={{ backgroundColor: 'black' }}>
-            <img style={{width: '5%', height: '5%'}} src={logo}/>
+            <Link className="navlink" to="/"><img className="logo" src={logo}/></Link>
           </div>
           <Switch>
             <Route exact path="/" render={()=><Home />} />
             <Route path="/about" render={()=><About />} />
             <Route path="/resume" render={()=><Resume />} />
+            <Route path="/contact" render={()=><ContactMe />} />
+            <Route path="/project" render={()=><Projects />} />
           </Switch>
         </div>
       </BrowserRouter>
