8a8f42da95c65522dd44473289502c83f1734f37
Lina Yang <t886515@hotmail.com>
2017-11-19 23:52:08 -0600

    minor server change

--- server/server.js
+++ server/server.js
@@ -46,8 +46,8 @@ app.get('/');
 //   res.send(path.join(__dirname, '../src/client/index.html'));
 // });
 
-const port = process.env.PORT || 8000;
+// const port = process.env.PORT || 8000;
 
-app.listen(port, () => {
-  console.log("Listening on port: ", port);
+app.listen(8000, () => {
+  console.log("Listening on port: ", 8000);
 });
